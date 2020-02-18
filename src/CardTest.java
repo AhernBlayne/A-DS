75b07e4c6d395361a8cbf3ca41d9cac344380229
blayne <blayneahern9@gmail.com>
2020-02-18 09:55:31 +0000

    completed the Deck class and created a driver class to test my Deck. Test class still needs to be completed though

---
+++ src/CardTest.java
@@ -0,0 +1,14 @@
+public class CardTest {
+    public static void main (String args []){
+        Deck d = new Deck();
+
+
+       System.out.println(d.toString());
+
+
+    }
+
+
+}
+
+
---
+++ src/Deck.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+    ArrayList<Card> Cards = new ArrayList<Card>();
+    String [] face = {"Hearts","Diamonds","Clubs","Spades"};
+    //{"Hearts","Diamonds","Clubs","Spades"};
+
+    public Deck(){
+        for(int y =0; y<=4 -1; y++) {
+            if(y ==0) {
+                for (int i = 1; i < 13; i++) {
+                    Card c = new Card(i, face[y]);
+                    Cards.add(c);
+
+                }
+            }
+            if(y == 1){
+                for (int i = 1; i < 13; i++) {
+                    Card c = new Card(i, face[y]);
+                    Cards.add(c);
+
+                }
+            }
+            if(y == 2){
+                for (int i = 1; i < 13 ; i++) {
+                    Card c = new Card(i, face[y]);
+                    Cards.add(c);
+
+                }
+
+            }
+            if(y == 3){
+                for (int i = 1; i < 13 ; i++) {
+                    Card c = new Card(i, face[y]);
+                    Cards.add(c);
+
+                }
+            }
+        }
+
+    }
+    public void shuffle(){
+        Collections.shuffle(Cards);
+
+    }
+
+
+    public String toString(){
+            return Cards.toString();
+
+    }
+}
