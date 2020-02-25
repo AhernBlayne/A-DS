908230f036529389a5f0a5fbbdd516b1d3400b82
blayne <blayneahern9@gmail.com>
2020-02-25 10:25:50 +0000

    new selectionSort class added

---
+++ src/selectionSort.java
@@ -0,0 +1,9 @@
+public class selectionSort {
+    public static void selectSort(int[] a){
+        // sorting in ascending order
+        for(int i =0; i <= a.length -2;i++){
+            // find index such that a[index] is the smallest of a[i] ..... [a.length-1]
+        }
+    }
+}
+    
